d9f427aadfec115db60b1609071009c26acd667c
Rap <aaa@aaa.net>
2018-07-27 14:57:26 +0900

    setup express js server

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+console.log('hello rap')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (reg, res) => {
+res.send({
+  message: 'hello world'
+})
+})
+
+app.listen(process.env.PORT || 8080)
